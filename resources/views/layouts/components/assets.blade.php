4f86e8182a566f017a1cf36f26373bb59b2cebb5
mahibashardeveloper <mahibashar2023@gmail.com>
2023-09-11 20:52:50 +0600

    resolve all problem with npm run build

--- resources/views/layouts/components/assets.blade.php
+++ resources/views/layouts/components/assets.blade.php
@@ -2,7 +2,9 @@
 <link rel="shortcut icon" href="img/favicon.ico" />
 
 <!-- Google Fonts -->
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
 
 <!-- Plugins -->
 <link rel="stylesheet" href="/css/plugin.css" />
