532f4ce1d61ceb9e58ec6ef215efa4f7aa668e8a
mahibashardeveloper <mahibashar2023@gmail.com>
2023-09-13 16:50:33 +0600

    update complete

--- resources/views/pages/widgets/banner-parallax.blade.php
+++ resources/views/pages/widgets/banner-parallax.blade.php
@@ -1,5 +1,20 @@
 <!-- parallax start-->
 <div class="parallax-container">
     <div style="background-image: url('{{ asset('/images/home/background-effect.jpg') }}')" class="img-fluid background-parallax"></div>
+    <div class="vh-100 d-flex justify-content-center align-items-center">
+        <div class="text-center">
+            <div class="row">
+                <div class="offset-lg-1 col-lg-10">
+                    <h1 class="text-white parallax-title animate__animated animate__fadeInRight">We Are Creative Digital Agency!</h1>
+                    <p class="text-white parallax-desc animate__animated animate__fadeInUp">We are a passionate digital design agency
+                        that specializes in beautiful and easy-to-use digital design &amp; web development
+                        services.</p>
+                    <a href="javascript:void(0)" class="btn mt-30 btn-theme">
+                        <span>Get started</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- parallax end-->
