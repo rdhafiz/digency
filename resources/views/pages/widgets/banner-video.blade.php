ef17a121e2eae4ad39cb84695e5ba8be21bd0e62
mahibashardeveloper <mahibashar2023@gmail.com>
2023-09-13 14:30:50 +0600

    video content responsive

--- resources/views/pages/widgets/banner-video.blade.php
+++ resources/views/pages/widgets/banner-video.blade.php
@@ -1,5 +1,5 @@
 <!-- video start-->
 <div class="w-100">
-    <video src="{{asset('video/banner-video.mp4')}}" autoplay loop class="img-fluid"></video>
+    <video src="{{asset('video/banner-video.mp4')}}" autoplay loop class="img-fluid w-100"></video>
 </div>
 <!-- video end-->
