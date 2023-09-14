29b0b86950c17cdd2a35284e70947b3713b15f43
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-09-14 17:25:06 +0600

    [14092023-theme-buttons]-button variations has made

---
+++ resources/views/pages/button-variations.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app')
+@section('title', 'Creative Digital Agency Template')
+@section('content')
+
+    <div class="button-section sec-wrap">
+        <div class="container">
+            <div class="sec-title text-center">Button Variations</div>
+
+
+            <div class="btn-section-content">
+                <div class="row text-center">
+                    <div class="col-12">
+                        <div class="border rounded-1 p-4">
+                            <button type="button" class="btn btn-theme m-1">Button theme</button>
+                            <button type="button" class="btn btn-outline-theme m-1">Button outline theme</button>
+                            <button type="button" class="btn btn-theme-gradient m-1">Button theme gradient</button>
+                            <button type="button" class="btn btn-primary-gradient m-1">Button primary gradient</button>
+                            <button type="button" class="btn btn-info-gradient m-1">Button info gradient</button>
+                            <button type="button" class="btn btn-danger-gradient m-1">Button danger gradient</button>
+                            <button type="button" class="btn btn-warning-gradient m-1">Button warning gradient</button>
+                            <button type="button" class="btn btn-spacial-icon m-1">Button spacial icon</button>
+                        </div>
+                        <div class="border text-start p-4">
+                            <!-- button theme-->
+                            <div class="sm-title">Button Theme</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-theme">Button theme</button>
+                            </xmp>
+                            <!-- button theme-->
+
+                            <!-- button outline theme-->
+                            <div class="sm-title">Button outline Theme</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-outline-theme">Button outline theme</button>
+                            </xmp>
+                            <!-- button outline theme-->
+
+                            <!-- button theme gradient-->
+                            <div class="sm-title">Button theme gradient</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-theme-gradient m-1">Button theme gradient</button>
+                            </xmp>
+                            <!-- button theme gradient-->
+
+                            <!-- button primary gradient-->
+                            <div class="sm-title">Button primary gradient</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-primary-gradient m-1">Button primary gradient</button>
+                            </xmp>
+                            <!-- button primary gradient-->
+
+                            <!-- button info gradient-->
+                            <div class="sm-title">Button info gradient</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-info-gradient m-1">Button info gradient</button>
+                            </xmp>
+                            <!-- button info gradient-->
+
+                            <!-- button danger gradient-->
+                            <div class="sm-title">Button danger gradient</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-danger-gradient m-1">Button danger gradient</button>
+                            </xmp>
+                            <!-- button danger gradient-->
+
+                            <!-- button warning gradient-->
+                            <div class="sm-title">Button Theme</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-warning-gradient m-1">Button warning gradient</button>
+                            </xmp>
+                            <!-- button warning gradient-->
+
+                            <!-- button spacial icon -->
+                            <div class="sm-title">button spacial icon</div>
+                            <xmp class="white-space-inherit code-line">
+                                <button type="button" class="btn btn-spacial-icon m-1">Button spacial icon</button>
+                            </xmp>
+                            <!-- button spacial icon -->
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/homepage-with-background-image', [FrontController::class, 'home_bac
 Route::get('/homepage-with-two-columns', [FrontController::class, 'home_two_column'])->name('home-two-columns');
 Route::get('/homepage-with-background-video', [FrontController::class, 'homePage_with_background_video'])->name('home-background-video');
 Route::get('/homepage-with-background-parallax-effect', [FrontController::class, 'homePage_with_background_parallax_effect'])->name('home-parallax-effect');
+Route::get('/button-variations', [FrontController::class, 'button_variations'])->name('button-variations');
