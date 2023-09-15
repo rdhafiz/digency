7930218db2d397e77af8411e122d9bb4e6c3ad9d
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-09-15 16:03:54 +0600

    [17]-4 alert added

--- resources/views/pages/alerts.blade.php
+++ resources/views/pages/alerts.blade.php
@@ -8,8 +8,49 @@
 
             <div class="alert-section-content">
                 <div class="border rounded-1 p-4">
+                    <!--Alert theme start-->
                     <div class="sm-title">ALert theme</div>
                     <div class="alert alert-theme">Alert theme</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-theme">Alert theme</div>
+                    </xmp>
+
+                    <!--Alert theme end  -->
+
+                    <hr>
+
+                    <!--Alert dark theme start-->
+                    <div class="sm-title">Alert dark theme</div>
+                    <div class="alert alert-theme-dark">Alert theme dark</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-theme-dark">Alert theme dark</div>
+                    </xmp>
+                    <!--Alert dark theme end  -->
+
+                    <hr>
+
+                    <!--Alert light theme start-->
+                    <div class="sm-title">Alert light theme</div>
+                    <div class="alert alert-theme-light">Alert theme light</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-theme-light">Alert theme light</div>
+                    </xmp>
+                    <!--Alert dark theme end  -->
+
+                    <hr>
+
+                    <!--Alert gradient theme start-->
+                    <div class="sm-title">Alert gradient theme</div>
+                    <div class="alert alert-theme-gradient">Alert theme gradient</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-theme-gradient">Alert theme light</div>
+                    </xmp>
+                    <!--Alert dark theme end  -->
+
                 </div>
             </div>
         </div>
