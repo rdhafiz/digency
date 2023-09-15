9ece8a0dd7d835951883367c11c08d26a86543c0
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-09-15 17:44:47 +0600

    [17]-all alerts has been added

--- resources/views/pages/alerts.blade.php
+++ resources/views/pages/alerts.blade.php
@@ -38,7 +38,7 @@
                     <xmp class="white-space-inherit code-line">
                         <div class="alert alert-theme-light">Alert theme light—check it out!</div>
                     </xmp>
-                    <!--Alert dark theme end  -->
+                    <!--Alert light theme end  -->
 
                     <hr>
 
@@ -49,7 +49,198 @@
                     <xmp class="white-space-inherit code-line">
                         <div class="alert alert-theme-gradient">Alert theme gradient—check it out!</div>
                     </xmp>
-                    <!--Alert dark theme end  -->
+                    <!--Alert gradient theme end  -->
+
+                    <hr>
+
+                    <!--Alert gradient theme animated start-->
+                    <div class="sm-title">Alert gradient theme animated</div>
+                    <div class="alert alert-theme-gradient-animated">Alert theme gradient animated—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-theme-gradient-animated">Alert theme gradient animated—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient theme animated end  -->
+
+                    <hr>
+
+                    <!--Alert primary dark start-->
+                    <div class="sm-title">Alert primary dark</div>
+                    <div class="alert alert-primary-dark">Alert primary dark—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-primary-dark">Alert primary dark—check it out!</div>
+                    </xmp>
+                    <!--Alert primary dark end  -->
+
+                    <hr>
+
+                    <!--Alert primary light start-->
+                    <div class="sm-title">Alert primary light</div>
+                    <div class="alert alert-primary-light">Alert primary light—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-primary-light">Alert primary light—check it out!</div>
+                    </xmp>
+                    <!--Alert primary light end  -->
+
+                    <hr>
+
+                    <!--Alert gradient primary start-->
+                    <div class="sm-title">Alert gradient theme</div>
+                    <div class="alert alert-primary-gradient">Alert primary gradient—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-primary-gradient">Alert primary gradient—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient primary end  -->
+
+                    <hr>
+
+                    <!--Alert gradient primary animated start-->
+                    <div class="sm-title">Alert gradient primary animated</div>
+                    <div class="alert alert-primary-gradient-animated">Alert primary gradient animated—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-primary-gradient-animated">Alert primary gradient animated—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient primary animated end  -->
+
+                    <hr>
+
+
+                    <!--Alert info dark start-->
+                    <div class="sm-title">Alert info dark</div>
+                    <div class="alert alert-info-dark">Alert info dark—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-info-dark">Alert info dark—check it out!</div>
+                    </xmp>
+                    <!--Alert info dark end  -->
+
+                    <hr>
+
+                    <!--Alert info light start-->
+                    <div class="sm-title">Alert info light</div>
+                    <div class="alert alert-info-light">Alert info light—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-info-light">Alert info light—check it out!</div>
+                    </xmp>
+                    <!--Alert info light end  -->
+
+                    <hr>
+
+                    <!--Alert gradient info start-->
+                    <div class="sm-title">Alert gradient theme</div>
+                    <div class="alert alert-info-gradient">Alert info gradient—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-info-gradient">Alert info gradient—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient info end  -->
+
+                    <hr>
+
+                    <!--Alert gradient info animated start-->
+                    <div class="sm-title">Alert gradient info animated</div>
+                    <div class="alert alert-info-gradient-animated">Alert info gradient animated—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-info-gradient-animated">Alert info gradient animated—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient info animated end  -->
+
+                    <hr>
+
+
+                    <!--Alert warning dark start-->
+                    <div class="sm-title">Alert warning dark</div>
+                    <div class="alert alert-warning-dark">Alert warning dark—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-warning-dark">Alert warning dark—check it out!</div>
+                    </xmp>
+                    <!--Alert warning dark end  -->
+
+                    <hr>
+
+                    <!--Alert warning light start-->
+                    <div class="sm-title">Alert warning light</div>
+                    <div class="alert alert-warning-light">Alert warning light—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-warning-light">Alert warning light—check it out!</div>
+                    </xmp>
+                    <!--Alert warning light end  -->
+
+                    <hr>
+
+                    <!--Alert gradient warning start-->
+                    <div class="sm-title">Alert gradient theme</div>
+                    <div class="alert alert-warning-gradient">Alert warning gradient—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-warning-gradient">Alert warning gradient—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient warning end  -->
+
+                    <hr>
+
+                    <!--Alert gradient warning animated start-->
+                    <div class="sm-title">Alert gradient warning animated</div>
+                    <div class="alert alert-warning-gradient-animated">Alert warning gradient animated—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-warning-gradient-animated">Alert warning gradient animated—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient warning animated end  -->
+
+                    <hr>
+
+                    <!--Alert danger dark start-->
+                    <div class="sm-title">Alert danger dark</div>
+                    <div class="alert alert-danger-dark">Alert danger dark—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-danger-dark">Alert danger dark—check it out!</div>
+                    </xmp>
+                    <!--Alert danger dark end  -->
+
+                    <hr>
+
+                    <!--Alert danger light start-->
+                    <div class="sm-title">Alert danger light</div>
+                    <div class="alert alert-danger-light">Alert danger light—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-danger-light">Alert danger light—check it out!</div>
+                    </xmp>
+                    <!--Alert danger light end  -->
+
+                    <hr>
+
+                    <!--Alert gradient danger start-->
+                    <div class="sm-title">Alert gradient theme</div>
+                    <div class="alert alert-danger-gradient">Alert danger gradient—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-danger-gradient">Alert danger gradient—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient danger end  -->
+
+                    <hr>
+
+                    <!--Alert gradient danger animated start-->
+                    <div class="sm-title">Alert gradient danger animated</div>
+                    <div class="alert alert-danger-gradient-animated">Alert danger gradient animated—check it out!</div>
+                    <div class="sm-title">HTML :</div>
+                    <xmp class="white-space-inherit code-line">
+                        <div class="alert alert-danger-gradient-animated">Alert danger gradient animated—check it out!</div>
+                    </xmp>
+                    <!--Alert gradient danger animated end  -->
+
+
 
                 </div>
             </div>
